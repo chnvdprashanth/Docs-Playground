c7638784486bea5b1631d508e7bf70bba67f605a
chnvdprashanth <chnvdprashanth@gmail.com>
2024-09-05 01:12:36 +0530

    updated local machine mongodb to mongodb atlas

--- server/src/utils/server.js
+++ server/src/utils/server.js
@@ -4,6 +4,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const connectMongoDB = async () => {
-    console.log("Mongodb Connected");
-  return mongoose.connect(`${process.env.MONGODB_URI}`);
+  console.log("Mongodb Connected");
+  return mongoose.connect(`${process.env.MONGODB_ATLAS_URI}`);
 };
